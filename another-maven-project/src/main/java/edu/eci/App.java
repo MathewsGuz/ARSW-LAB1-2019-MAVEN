5de0aba59fa7a0fa4fef9d11f41c86664b4cfaa3
Julian Mateo Guzman Diaz <matguz96@hotmail.com>
2019-01-21 23:50:49 -0500

    2 maven proyects plugin

---
+++ another-maven-project/src/main/java/edu/eci/App.java
@@ -0,0 +1,12 @@
+package edu.eci;
+
+import edu.eci.FileSpy;
+
+public class App 
+{
+    public static void main( String[] args )throws Exception
+    {
+		System.out.print("we are in app");
+        FileSpy.main(args);
+    }
+}
